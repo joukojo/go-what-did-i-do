fa3733560aa31a9dde1a7c2f59d81e869d3d326d
Jouko Johansson <76950886+netum-jouko@users.noreply.github.com>
2025-08-11 13:50:13 +0300

    Refactor customersAdd command to improve error handling and remove copyright comment

--- cmd/customersAdd.go
+++ cmd/customersAdd.go
@@ -1,11 +1,11 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
+// Package cmd contains all command line options
 package cmd
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/joukojo/go-what-did-i-do/promptutil"
 	"github.com/joukojo/go-what-did-i-do/services"
 	"github.com/spf13/cobra"
 )
@@ -20,17 +20,30 @@ var customersAddCmd = &cobra.Command{
 	Short: "Adds a new customer",
 	Long: `This command requires the following parameters: 
 	- name: The name of the customer`,
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		// Import the Customer type from the appropriate package
+
+		if customerName == "" {
+			fmt.Println("Please enter the customer's name:")
+			customerName = promptutil.AskString("Name: ")
+		}
+		if customerName == "" {
+			fmt.Println("Error: Customer name cannot be empty.")
+			_ = cmd.Help()
+			return
+		}
 		services.CustomerStorage.Add(services.Customer{
 			ID:   time.Now().Unix(),
 			Name: customerName,
 		})
-		services.CustomerStorage.Save()
+		err := services.CustomerStorage.Save()
+		if err != nil {
+			fmt.Println("Error saving customer:", err)
+		}
 	},
 }
 
 func init() {
 	customersCmd.AddCommand(customersAddCmd)
-	customersAddCmd.Flags().StringVar(&customerName, "name", "", "Name of the project")
+	customersAddCmd.Flags().StringVar(&customerName, "name", "", "Name of the customer")
 }
